4e06cc319a0492f77fb73eaa69658dd06183a5ea
jayadikurniawan <jayadikurniawan27@gmail.com>
2023-06-22 13:24:31 +0700

    tes

--- controller/c_pemberiKerja_update.php
+++ controller/c_pemberiKerja_update.php
@@ -3,7 +3,7 @@ session_start();
 require '../model/m_konek.php';
 
 if (isset($_POST['pelamar']) && isset($_POST['idpekerjaan']) && isset($_POST['status'])) {
-    
+
     $pelamar = $_POST['pelamar'];
     $idpekerjaan = $_POST['idpekerjaan'];
     $status = $_POST['status'];
@@ -12,6 +12,7 @@ if (isset($_POST['pelamar']) && isset($_POST['idpekerjaan']) && isset($_POST['st
     $query = "UPDATE listpelamar SET status = '$status' WHERE pelamar = '$pelamar' AND idpekerjaan = '$idpekerjaan'";
 
     if ($mysqli->query($query)) {
+        header("Location: ../view/v_pemberikerja_list.php");
         echo "Status berhasil diperbarui!";
     } else {
         echo "Gagal memperbarui status: " . $mysqli->error;
